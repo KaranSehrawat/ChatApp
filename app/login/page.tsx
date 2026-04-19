8803256696102bef3936f082a97ce73bbf83d8a5
Karan Sehrawat <azcreations2688@gmail.com>
2026-04-19 11:47:34 +0530

    2nd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
-import { Mail } from "lucide-react";
+"use client";
+import React, { useState, } from 'react';
+import { ArrowRight, Mail } from "lucide-react";
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 const LoginPage = () => {
+  const [email, setEmail] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+    setLoading(true);
+
+    try{
+      const {data} = await axios.post('http://localhost:5000/api/v1/login', { email });
+      alert(data.message)
+      router.push('/verify?email=${email}');
+    } catch(error: any){
+      alert(error.response.data.message)
+    } finally{
+      setLoading(false)
+    }
+  };
   return (
     <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
       <div className="min-w-md w-full">
@@ -17,10 +38,15 @@ const LoginPage = () => {
             <p className="text-gray-300 text-lg">Enter your email to continue your journey</p>
           </div>
 
-          <form className='space-y-6'>
+          <form onSubmit={handleSubmit} className='space-y-6'>
             <div>
               <label htmlFor='email' className='block text-sm font-medium text-gray-300 mb-2'>Email Address</label>
+              <input type="email" id="email" className='w-full px-4 py-4 bg-gray-400 borger-gray-600 rounded-lg text-white placeholder-gray-400' placeholder="Enter your email address" value={email} onChange={(e) => setEmail(e.target.value)} required />
             </div>
+            <button type="submit" className='w-full bg-blue-600 text-white py-4 px-6 rounded-lg font-semibold hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed'>
+              <div className='felx items-center justify-center gap-2'>
+                <span>Send Verification Code</span>
+                <ArrowRight className='w-5 h-5'/></div></button>
           </form>
         </div>
       </div>
